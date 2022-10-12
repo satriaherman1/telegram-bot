09f4b39d4f5df9d4bc0e95445bc90afc851461fd
satriaherman1 <muhammadsatriaherman@gmail.com>
2022-10-12 09:29:39 +0700

    add schedule and image jokes

--- bot.js
+++ bot.js
@@ -1,57 +1,65 @@
 // require your telegram bot API Key in .env
-require("dotenv").config()
+require("dotenv").config();
 const token = process.env.BOT_TOKEN;
 
 const telegraf = require("telegraf");
 const { Command } = require("./command.js");
-const cmd = require('node-cmd')
-const command = new Command()
+const cmd = require("node-cmd");
+const command = new Command();
 
 const bot = new telegraf(token);
 
-
 // command
-bot.start( (ctx) => ctx.reply("hi"))
-bot.help( (ctx) => ctx.reply("this is dirandra bot"))
-bot.command( "desc", ctx => ctx.reply("this bot development by satria"))
-bot.command( "myprofile" , ( ctx ) => {
-    const profile = ctx.from 
+bot.start((ctx) => ctx.reply("hi"));
+bot.help((ctx) => ctx.reply("this is dirandra bot"));
+bot.command("desc", (ctx) => ctx.reply("this bot development by satria"));
+bot.command("myprofile", (ctx) => {
+  const profile = ctx.from;
 
-    ctx.reply(
-        `your name : ${profile["first_name"]}
+  ctx.reply(
+    `your name : ${profile["first_name"]}
          your username : ${profile["username"]},
          are you a bot? : ${profile["bot"] ? "yes" : "no"},
          your country code : ${profile["language_code"]}
-        `
-        
-        
-        ) })
-bot.command("deleteMessage" , ctx => ctx.deleteMessage())
-bot.command("whoami", (ctx) => {
-    ctx.replyWithPhoto("https://i.pinimg.com/originals/a0/1c/46/a01c46ba0109b58d5335548f8d44c718.jpg")
-    ctx.reply("it's you")
-})
-bot.command("/shutdown" , ( ctx ) => {
-    ctx.reply("processing to shutdown your pc")
-    cmd.run("shutdown /s /t 10")
+        `,
+  );
 });
-bot.command('/jokes' , async (ctx) => {
-    ctx.reply('tunggu sebentar ya')
-    ctx.reply(await command.joke())
-    ctx.reply('xixixi')
-})
+bot.command("deleteMessage", (ctx) => ctx.deleteMessage());
+bot.command("whoami", (ctx) => {
+  ctx.replyWithPhoto("https://i.pinimg.com/originals/a0/1c/46/a01c46ba0109b58d5335548f8d44c718.jpg");
+  ctx.reply("it's you");
+});
+
+bot.command("/shutdown", (ctx) => {
+  ctx.reply("processing to shutdown your pc");
+  cmd.run("shutdown /s /t 10");
+});
+
+bot.command("/jokes", async (ctx) => {
+  ctx.reply("tunggu sebentar ya");
+  ctx.reply(await command.joke());
+  ctx.reply("xixixi");
+});
+
+bot.command("/jokes_gambar", async (ctx) => {
+  ctx.reply("tunggu sebentar ya");
+  ctx.replyWithPhoto(await command.jokeImage());
+  ctx.reply("xixixi");
+});
+
+bot.command("/jadwal", async (ctx) => {
+  ctx.reply(command.schedule());
+  ctx.reply(`
+  Semangat Kuliah yaa ^_^
+  `);
+});
 
 // recheived message
-bot.on('text' , ( ctx ) => { 
-    
-    ctx.reply(`hello ${ctx.state.role}`) 
-    
-    
+bot.on("text", (ctx) => {
+  ctx.reply(`hello ${ctx.state.role}`);
 });
-bot.on(["photo" ,"video"], context => context.replyWithPhoto("data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="))
-bot.on("sticker" ,ctx => ctx.replyWithPhoto("https://i.pinimg.com/originals/a0/1c/46/a01c46ba0109b58d5335548f8d44c718.jpg"))
-
-
+bot.on(["photo", "video"], (context) => context.replyWithPhoto("data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="));
+bot.on("sticker", (ctx) => ctx.replyWithPhoto("https://i.pinimg.com/originals/a0/1c/46/a01c46ba0109b58d5335548f8d44c718.jpg"));
 
 bot.launch();
 
--- command.js
+++ command.js
@@ -1,15 +1,34 @@
-const axios = require('axios')
-require("dotenv").config()
+const axios = require("axios");
+require("dotenv").config();
 
-class Command{
-   constructor(){
+class Command {
+  constructor() {}
 
-   }
-
-   async joke(){
-    const jokes=  await axios.get(`${process.env.JOKE_API}/api/text/random`)
+  async joke() {
+    const jokes = await axios.get(`${process.env.JOKE_API}/api/text/random`);
     return jokes.data.data;
-   }
+  }
+
+  schedule() {
+    return `Senin, 12:30 - 14:10 = Dasar Sistem Informasi
+Rabu, 10:00 - 12:30 = Dasar Infrastruktur Teknologi Informasi
+Rabu, 12:30 - 14:10 = Bahasa Inggris
+Rabu, 14:30 - 15:50 = Matematika Diskrit
+Kamis, 07:30 - 09:10 = Data Science For Business
+Kamis, 10:00 - 12:30 = Dasar-Dasar Pemrograman
+Kamis, 14:10 - 15:50 = Pengantar Bisnis dan Manajemen
+Jum'at, 07:30 - 09:10 = Pancasila
+Sabtu, 08:30 - 10:00 = Aplikasi Komputer Bisnis
+
+
+
+     `;
+  }
+
+  async jokeImage() {
+    const jokes = await axios.get(`${process.env.JOKE_API}/api/image/random`);
+    return jokes.data.data.url;
+  }
 }
 
-module.exports = {Command}
+module.exports = { Command };
